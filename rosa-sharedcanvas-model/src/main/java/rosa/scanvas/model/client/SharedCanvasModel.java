b63130a4159c098e1c2ac4fe2c646033b118d130
John Abrahams <jabrah23@gmail.com>
2013-03-04 20:19:07 +0000

    created modules: rosa-sharedcanvas-demo-website, rosa-sharedcanvas-model, rosa-sharedcanvas-viewer

---
+++ rosa-sharedcanvas-model/src/main/java/rosa/scanvas/model/client/SharedCanvasModel.java
@@ -0,0 +1,16 @@
+package rosa.scanvas.model.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class SharedCanvasModel implements EntryPoint {
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+	  
+  }
+}
---
+++ rosa-sharedcanvas-viewer/src/main/java/rosa/scanvas/viewer/client/SharedCanvasViewer.java
@@ -0,0 +1,16 @@
+package rosa.scanvas.viewer.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class SharedCanvasViewer implements EntryPoint {
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+	  
+  }
+}
