654e86dee38223aa77fb1fbbc033c2c30fb9662d
Mark Patton <mpatton@jhu.edu>
2013-04-24 19:55:54 +0000

    Fixed up Home presenter and view

--- rosa-sharedcanvas-demo-website/src/main/java/rosa/scanvas/demo/website/client/presenter/HomePanelPresenter.java
+++ rosa-sharedcanvas-demo-website/src/main/java/rosa/scanvas/demo/website/client/presenter/HomePanelPresenter.java
@@ -1,8 +1,12 @@
 package rosa.scanvas.demo.website.client.presenter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rosa.scanvas.demo.website.client.PanelData;
 import rosa.scanvas.demo.website.client.PanelState;
 import rosa.scanvas.demo.website.client.PanelView;
+import rosa.scanvas.demo.website.client.event.PanelDisplayedEvent;
 import rosa.scanvas.demo.website.client.event.PanelRequestEvent;
 
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -13,79 +17,117 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * Presents a list of manifest collections which a user can select. The user can
+ * entire the url to a manifest collection or a manifest directly.
+ */
 public class HomePanelPresenter implements PanelPresenter {
-    // TODO Change this to have list of collections
-
     public interface Display extends IsWidget {
-        Anchor getRoseDataLink();
+        HasClickHandlers getCollectionList();
 
-        Anchor getTestDataLink();
+        int getSelectedCollection();
 
-        HasClickHandlers getGoButton();
+        void setData(List<String> names);
+
+        HasClickHandlers getLoadButton();
 
         HasValue<String> getUserUrlText();
 
         HasKeyUpHandlers getUserUrlKeyUpHandlers();
 
+        HasValue<Boolean> getUserUrlIsCollection();
+
         void resize(int width, int height);
     }
 
+    private static final List<String> col_titles;
+    private static final List<String> col_urls;
+
+    static {
+        col_titles = new ArrayList<String>();
+        col_urls = new ArrayList<String>();
+
+        col_titles.add("Roman de la Rose Digital library");
+        col_urls.add("http://rosetest.library.jhu.edu/sc");
+
+        col_titles.add("Test data");
+        col_urls.add("http://rosetest.library.jhu.edu/sctest");
+    }
+
     private final Display display;
-    private final HandlerManager eventBus;
+    private final HandlerManager event_bus;
     private final int panel_id;
 
     public HomePanelPresenter(Display display, HandlerManager eventBus,
             int panel_id) {
         this.display = display;
-        this.eventBus = eventBus;
+        this.event_bus = eventBus;
         this.panel_id = panel_id;
 
-        display.getRoseDataLink().setHTML(
-                "<i>Roman de la Rose</i> Digital library");
-        display.getTestDataLink().setText("Collection of test data");
-
         bind();
+
+        display.setData(col_titles);
     }
 
     private void bind() {
-        display.getRoseDataLink().addClickHandler(new ClickHandler() {
+        display.getCollectionList().addClickHandler(new ClickHandler() {
             @Override
-            public void onClick(ClickEvent click_event) {
-                PanelState state = new PanelState(
-                        PanelView.MANIFEST_COLLECTION,
-                        "http://rosetest.library.jhu.edu/sc");
-                PanelRequestEvent event = new PanelRequestEvent(
-                        PanelRequestEvent.PanelAction.CHANGE, panel_id, state);
-                eventBus.fireEvent(event);
+            public void onClick(ClickEvent event) {
+                doCollectionLoad(display.getSelectedCollection());
             }
         });
 
-        display.getGoButton().addClickHandler(new ClickHandler() {
+        display.getLoadButton().addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                doGoClick();
+                doUserLoad();
             }
         });
 
         display.getUserUrlKeyUpHandlers().addKeyUpHandler(new KeyUpHandler() {
             public void onKeyUp(KeyUpEvent event) {
                 if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    doGoClick();
+                    doUserLoad();
                 }
             }
         });
     }
 
-    private void doGoClick() {
-        // TODO the case of real data: when someone puts in a real URL
-        // data will be read from URL, then it must be determined if it is a
-        // collection of manifests
-        // or a single manifest
-        // String url = display.getUserUrlText().getValue();
+    private void doCollectionLoad(int sel) {
+        if (sel < 0) {
+            return;
+        }
+
+        PanelState state = new PanelState(PanelView.MANIFEST_COLLECTION,
+                col_urls.get(sel));
+        PanelRequestEvent event = new PanelRequestEvent(
+                PanelRequestEvent.PanelAction.CHANGE, panel_id, state);
+        event_bus.fireEvent(event);
+    }
+
+    private void doUserLoad() {
+        String url = display.getUserUrlText().getValue();
+
+        if (url.isEmpty()) {
+            return;
+        }
+
+        boolean is_col = display.getUserUrlIsCollection().getValue();
+
+        PanelState state;
+
+        if (is_col) {
+            state = new PanelState(PanelView.MANIFEST_COLLECTION, url);
+        } else {
+            state = new PanelState(PanelView.MANIFEST, url);
+        }
+
+        PanelRequestEvent event = new PanelRequestEvent(
+                PanelRequestEvent.PanelAction.CHANGE, panel_id, state);
+        event_bus.fireEvent(event);
     }
 
     @Override
@@ -95,6 +137,8 @@ public class HomePanelPresenter implements PanelPresenter {
 
     @Override
     public void display(PanelData data) {
+        PanelDisplayedEvent event = new PanelDisplayedEvent(panel_id, data);
+        event_bus.fireEvent(event);
     }
 
     @Override
--- rosa-sharedcanvas-demo-website/src/main/java/rosa/scanvas/demo/website/client/view/HomeView.java
+++ rosa-sharedcanvas-demo-website/src/main/java/rosa/scanvas/demo/website/client/view/HomeView.java
@@ -1,73 +1,73 @@
 package rosa.scanvas.demo.website.client.view;
 
+import java.util.List;
+
 import rosa.scanvas.demo.website.client.presenter.HomePanelPresenter;
 
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.dom.client.HasKeyUpHandlers;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class HomeView extends Composite implements HomePanelPresenter.Display {
-    private final Anchor roseDataLink;
-    private final Anchor testDataLink;
-    private final Button goButton;
-    private final TextBox userUrlText;
-
-    // TODO use listbox for collections
+    private final ListBox col_listbox;
+    private final Button load_button;
+    private final TextBox user_textbox;
+    private final CheckBox is_col_checkbox;
 
     public HomeView() {
-        this.roseDataLink = new Anchor();
-        this.testDataLink = new Anchor();
-        this.goButton = new Button("Load");
-        this.userUrlText = new TextBox();
+        this.col_listbox = new ListBox();
+        this.load_button = new Button("Load");
+        this.user_textbox = new TextBox();
+        this.is_col_checkbox = new CheckBox("Collection");
 
         FlowPanel main = new FlowPanel();
 
-        main.add(new Label("Choose a collection to access"));
-
-        // TODO put these in a list box
+        Label panel_title = new Label("Choose a collection to access.");
+        panel_title.setStylePrimaryName("PanelTitle");
         
-        main.add(roseDataLink);
-        main.add(testDataLink);
+        main.add(panel_title);
+        main.add(col_listbox);
 
-        FlowPanel go_panel = new FlowPanel();
+        col_listbox.setVisibleItemCount(5);
 
-        main.add(new Label("Access data from URL"));
-        go_panel.add(userUrlText);
-        go_panel.add(goButton);
+        FlowPanel toolbar_panel = new FlowPanel();
+        toolbar_panel.setStylePrimaryName("PanelToolbar");
+        
+        Label panel_header = new Label("Access data from URL.");
+        panel_header.setStylePrimaryName("PanelHeader");
+        
+        main.add(panel_header);
+        main.add(new Label(
+                "Indicate whether it is a collection of manifest or an individual manifest with the checkbox."));
 
-        main.add(go_panel);
+        main.add(toolbar_panel);
+
+        toolbar_panel.add(user_textbox);
+        toolbar_panel.add(is_col_checkbox);
+        toolbar_panel.add(load_button);
+
         main.setStylePrimaryName("PanelView");
 
         initWidget(main);
     }
 
-    public Anchor getRoseDataLink() {
-        return roseDataLink;
-    }
-
-    public Anchor getTestDataLink() {
-        return testDataLink;
-    }
-
-    public HasClickHandlers getGoButton() {
-        return goButton;
-    }
-
     public HasValue<String> getUserUrlText() {
-        return userUrlText;
+        return user_textbox;
     }
 
     public HasKeyUpHandlers getUserUrlKeyUpHandlers() {
-        return userUrlText;
+        return user_textbox;
     }
 
+    @Override
     public Widget asWidget() {
         return this;
     }
@@ -75,5 +75,34 @@ public class HomeView extends Composite implements HomePanelPresenter.Display {
     @Override
     public void resize(int width, int height) {
         setPixelSize(width, height);
+    }
+
+    @Override
+    public HasClickHandlers getCollectionList() {
+        return col_listbox;
+    }
+
+    @Override
+    public int getSelectedCollection() {
+        return col_listbox.getSelectedIndex();
+    }
+
+    @Override
+    public void setData(List<String> names) {
+        col_listbox.clear();
+
+        for (String name : names) {
+            col_listbox.addItem(name);
+        }
+    }
+
+    @Override
+    public HasClickHandlers getLoadButton() {
+        return load_button;
+    }
+
+    @Override
+    public HasValue<Boolean> getUserUrlIsCollection() {
+        return is_col_checkbox;
     }
 }
